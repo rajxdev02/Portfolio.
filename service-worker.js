dece2d972435b1aba1ed8f62fa5592cf637cbc83
rajxdev02 <rajbodade0210@gmail.com>
2025-07-20 18:56:40 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const cacheName = 'raj-portfolio-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
